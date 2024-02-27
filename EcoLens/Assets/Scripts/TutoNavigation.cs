53add65e837691494a4be01ae2c31c2fe3b6aef3
Abel Henry-Lapassat <abbel.haol@gmail.com>
2024-02-27 10:15:21 +0100

    implemented TutoPane

--- EcoLens/Assets/Scripts/TutoNavigation.cs
+++ EcoLens/Assets/Scripts/TutoNavigation.cs
@@ -16,7 +16,15 @@ public class TutoNavigation : MonoBehaviour{
     public GameObject nav;
     public GameObject manip;
     public GameObject capsule;
+    public GameObject feature1;
+    public GameObject feature2;
+    public GameObject feature3;
+    public GameObject feature4;
+    public GameObject feature5;
+    public GameObject feature6;
     public GameObject scores;
+    public GameObject out_count;
+    public GameObject trash_count;
     public GameObject thanks;
 
     //Sounds
@@ -24,7 +32,7 @@ public class TutoNavigation : MonoBehaviour{
     public AudioSource click_sound;
 
     //localizer
-    private string current = ""; //"w", "g", "n", "m", "c", "s", "t"
+    private string current = ""; //"w", "g", "n", "m", "c", "s", "t", "f1-6", "oc", "tc"
     
     public void Start(){
         current = "w";
@@ -62,8 +70,32 @@ public class TutoNavigation : MonoBehaviour{
             case "c":
                 capsule.SetActive(false);
                 break;
+            case "f1":
+                feature1.SetActive(false);
+                break;
+            case "f2":
+                feature2.SetActive(false);
+                break;
+            case "f3":
+                feature3.SetActive(false);
+                break;
+            case "f4":
+                feature4.SetActive(false);
+                break;
+            case "f5":
+                feature5.SetActive(false);
+                break;
+            case "f6":
+                feature6.SetActive(false);
+                break;
             case "s":
                 scores.SetActive(false);
+                break;
+            case "oc":
+                out_count.SetActive(false);
+                break;
+            case "tc":
+                trash_count.SetActive(false);
                 break;
             
             default:
@@ -98,16 +130,56 @@ public class TutoNavigation : MonoBehaviour{
                 manip.SetActive(true);
                 current = "m";
                 break;
-            case "s":
-                scores.SetActive(false);
+            case "f1":
+                feature1.SetActive(false);
                 capsule.SetActive(true);
                 current = "c";
                 break;
+            case "f2":
+                feature2.SetActive(false);
+                feature1.SetActive(true);
+                current = "f1";
+                break;
+            case "f3":
+                feature3.SetActive(false);
+                feature2.SetActive(true);
+                current = "f2";
+                break;
+            case "f4":
+                feature4.SetActive(false);
+                feature3.SetActive(true);
+                current = "f3";
+                break;
+            case "f5":
+                feature5.SetActive(false);
+                feature4.SetActive(true);
+                current = "f4";
+                break;
+            case "f6":
+                feature6.SetActive(false);
+                feature5.SetActive(true);
+                current = "f5";
+                break;
+            case "s":
+                scores.SetActive(false);
+                feature6.SetActive(true);
+                current = "f6";
+                break;
+            case "oc":
+                out_count.SetActive(false);
+                scores.SetActive(true);
+                current = "s";
+                break;
+            case "tc":
+                trash_count.SetActive(false);
+                out_count.SetActive(true);
+                current = "oc";
+                break;
             case "t":
                 thanks.SetActive(false);
-                scores.SetActive(true);
+                trash_count.SetActive(true);
                 next.SetActive(true);
-                current = "s";
+                current = "tc";
                 break;
             
             default:
@@ -140,16 +212,56 @@ public class TutoNavigation : MonoBehaviour{
                 break;
             case "c":
                 capsule.SetActive(false);
+                feature1.SetActive(true);
+                current = "f1";
+                break;
+            case "f1":
+                feature1.SetActive(false);
+                feature2.SetActive(true);
+                current = "f2";
+                break;
+            case "f2":
+                feature2.SetActive(false);
+                feature3.SetActive(true);
+                current = "f3";
+                break;
+            case "f3":
+                feature3.SetActive(false);
+                feature4.SetActive(true);
+                current = "f4";
+                break;
+            case "f4":
+                feature4.SetActive(false);
+                feature5.SetActive(true);
+                current = "f5";
+                break;
+            case "f5":
+                feature5.SetActive(false);
+                feature6.SetActive(true);
+                current = "f6";
+                break;
+            case "f6":
+                feature6.SetActive(false);
                 scores.SetActive(true);
                 current = "s";
                 break;
             case "s":
                 scores.SetActive(false);
+                out_count.SetActive(true);
+                current = "oc";
+                break;
+            case "oc":
+                out_count.SetActive(false);
+                trash_count.SetActive(true);
+                current = "tc";
+                break;
+            case "tc":
+                trash_count.SetActive(false);
                 thanks.SetActive(true);
                 next.SetActive(false);
                 current = "t";
                 break;
-            
+
             default:
                 break;
         }
